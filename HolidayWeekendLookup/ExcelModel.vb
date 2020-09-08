36e5f9fdab29fdedd206f100483e3c570ca935a2
jortega33 <jacob.ortega@dxc.com>
2020-09-08 10:23:08 +0800

    Published Holiday and Weekup Lookup

---
+++ HolidayWeekendLookup/ExcelModel.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Configuration
+Imports System.Data
+Imports System.IO
+Imports System.Reflection
+Imports System.Text
+Imports ExcelDataReader
+
+Public Class ExcelModel
+    Private DTCollection As DataTableCollection
+
+    Public Sub Connect()
+        Try
+            ' Get file full location and read it
+            Dim AssemblyName As String = Assembly.GetExecutingAssembly.GetModules()(0).FullyQualifiedName
+            Dim FilePath As String = Path.GetDirectoryName(AssemblyName) & ConfigurationManager.AppSettings.Get("ExcelFile")
+            Dim FStream As FileStream = File.Open(FilePath, FileMode.Open, FileAccess.Read)
+
+            ' Create Excel reader from stream
+            Dim ExcelDataReader As IExcelDataReader
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance)
+            If Path.GetExtension(FilePath).ToUpper() = ".XLS" Then
+                ExcelDataReader = ExcelReaderFactory.CreateReader(FStream)
+            Else
+                ExcelDataReader = ExcelReaderFactory.CreateOpenXmlReader(FStream)
+            End If
+
+            ' Configure Excel to DataSet
+            Dim Result As DataSet = ExcelDataReader.AsDataSet(New ExcelDataSetConfiguration() With {
+                .ConfigureDataTable = Function(__) New ExcelDataTableConfiguration() With {
+                .UseHeaderRow = True}})
+            DTCollection = Result.Tables()
+        Catch ex As Exception
+            Console.WriteLine("Exception: " & ex.ToString())
+            Environment.Exit(-1)
+        End Try
+    End Sub
+
+    Public Function CheckIfHoliday(ByVal Dt As Date, ByVal Loc As String)
+        Dim Sheetname = Dt.Year.ToString
+        For Each Table As DataTable In DTCollection
+            If (Table.TableName = Sheetname) Then
+                Dim DRow As DataRow() = Table.Select("Date = '" & Dt & "' AND Country = '" & Loc & "'")
+                Return If(DRow.Length > 0, True, False)
+            End If
+        Next
+
+        Return False
+    End Function
+End Class
--- HolidayWeekendLookup/Program.vb
+++ HolidayWeekendLookup/Program.vb
@@ -1,24 +1,41 @@
 Imports CommandLine
+Imports System.DayOfWeek
 
 Module Program
+    Dim ExcelModel = New ExcelModel
+
     Sub Main(ByVal args As String())
         Dim Options = New Options
 
-        Dim unused = CommandLine.Parser.Default.ParseArguments(Of Options)(args) _
-        .WithParsed(Function(opts As Options) _EndedOnHolidayOrWeekend(opts)) _
+        CommandLine.Parser.Default.ParseArguments(Of Options)(args) _
+        .WithParsed(Sub(opts As Options)
+                        Console.WriteLine(_EndedOnHolidayOrWeekend(opts))
+                    End Sub) _
         .WithNotParsed(Function(errs As IEnumerable(Of [Error])) 1)
     End Sub
 
-    Private Function _EndedOnHolidayOrWeekend(ByVal opts As Options)
-        Dim excelConnection = New ExcelConnection
-        Dim SLA = opts.SLA
-        Dim Location = opts.Location
-        Dim DateTime = opts.DateTime
+    Private Function _EndedOnHolidayOrWeekend(ByVal opts As Options) As Boolean
+        Dim SLA As Integer = opts.SLA
+        Dim Location As String = opts.Location
+        Dim DateTime As DateTime = opts.DateTime
+        Dim DateEndOfSLA As DateTime = DateTime.AddHours(SLA)
 
-        Console.WriteLine(SLA)
-        Console.WriteLine(Location)
-        Console.WriteLine(DateTime)
+        ' Access Excel Holiday Lookup
+        ExcelModel.Connect
 
-        Return 0
+        ' Check if date when the SLA will run out is a holiday or a weekend.
+        If (_IsWeekend(DateEndOfSLA) Or _IsHoliday(DateEndOfSLA, Location)) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Private Function _IsWeekend(ByVal dt As DateTime) As Boolean
+        Return If(dt.DayOfWeek = DayOfWeek.Saturday Or dt.DayOfWeek = DayOfWeek.Sunday, True, False)
+    End Function
+
+    Private Function _IsHoliday(ByVal Dt As DateTime, ByVal Loc As String) As Boolean
+        Return ExcelModel.CheckIfHoliday(Dt.ToShortDateString, Loc)
     End Function
 End Module
